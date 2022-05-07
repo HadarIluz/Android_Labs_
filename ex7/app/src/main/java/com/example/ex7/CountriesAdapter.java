80d26d40f555d0c630a1dfcad2927be2a4299f4d
iluzhadar <iluzhadar@gmail.com>
2022-05-07 21:53:20 +0300

    create update list, long press on each row delete it.

--- ex7/app/src/main/java/com/example/ex7/CountriesAdapter.java
+++ ex7/app/src/main/java/com/example/ex7/CountriesAdapter.java
@@ -1,7 +1,15 @@
 package com.example.ex7;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,7 +19,8 @@ import java.util.List;
 public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.CountriesViewHolder> {
 
     private final List<Country> countriesList;
-    private boolean selectedRow = false;
+    //private static int countryListSize=0;
+    private int selectedRow = -1;
 
     public CountriesAdapter(Context context) {
         this.countriesList = CountryXMLParser.parseCountries(context);
@@ -19,17 +28,92 @@ public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.Coun
 
     @NonNull
     @Override
-    public CountriesAdapter.CountriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CountriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View countryView = inflater.inflate(R.layout.country_row_frag, parent, false);
+        return new CountriesViewHolder(countryView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CountriesAdapter.CountriesViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CountriesViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.bindData(countriesList.get(position));
 
+        // This listener will change the color of selected row
+        holder.row_linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedRow = position;
+                notifyDataSetChanged();
+            }
+        });
+
+        if(selectedRow == position){
+            holder.row_linearLayout.setBackgroundColor(Color.parseColor("#03dffc"));
+        }else{
+            holder.row_linearLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return countriesList.size();
     }
+
+    // Each row in RecyclerView will get reference of this CountriesViewHolder
+    // *** Include the function that can remove the row
+    public class CountriesViewHolder extends RecyclerView.ViewHolder
+    {
+        private final Context   context;
+        private final View      countryItem;
+        private final ImageView flagImageView;
+        private final TextView nameTextView;
+        private final TextView  populationTextView;
+        private LinearLayout row_linearLayout;
+
+        public CountriesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            context             = itemView.getContext();
+            countryItem         = itemView.findViewById(R.id.country_row_frag);
+            flagImageView       = itemView.findViewById(R.id.flagImageView);
+            nameTextView        = itemView.findViewById(R.id.countryNameTextView);
+            populationTextView  = itemView.findViewById(R.id.countryPopulationTextView);
+            row_linearLayout        = itemView.findViewById(R.id.country_row_frag);
+            //*****************************************************************************************
+            // This listener function call the function removeItem -> wil remove item from the ArrayList***
+            //**************************************************************************************
+            countryItem.setOnLongClickListener(view -> {
+                removeItem(getAdapterPosition());
+                return true;
+            });
+
+
+        }
+
+        //******** This function bind\connect the row widgets with the data
+        public void bindData(Country country) {
+            flagImageView.setImageResource(getDrawableId(context, country.getFlag()));
+            nameTextView.setText(country.getName());
+            populationTextView.setText(country.getShorty());
+        }
+
+        private void removeItem(int position) {
+            countriesList.remove(position);
+            notifyItemRemoved(position);// ****** this notify to the recycle that there is change in data base
+        }
+    }
+
+    private static int getDrawableId(Context context, String drawableName) {
+        Resources resources = context.getResources();
+        return resources.getIdentifier(drawableName, "drawable", context.getPackageName());
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- ex7/app/src/main/java/com/example/ex7/CountryFragment.java
+++ ex7/app/src/main/java/com/example/ex7/CountryFragment.java
@@ -2,39 +2,25 @@ package com.example.ex7;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CountryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CountryFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+
+public class CountryFragment extends Fragment  {
 
     private RecyclerView recyclerView;
-    CountryFragmentListener countryFragmentListener; // hold the mainActivity reference.
+    CountriesFragmentListener listener; // hold the mainActivity reference
     private CountriesAdapter countriesAdapter;
 
-    /*This function connect our mainActivity with the B fragment
-    when the context var is the mainActivity*/
     @Override
-    public void onAttach(@NonNull Context context) {
-        try{
-            this.countryFragmentListener = (CountryFragmentListener)context;
-        }catch(ClassCastException e){
-            throw new ClassCastException("the class " +
-                    getActivity().getClass().getName() +
-                    " must implements the interface 'FragBListener'");
-        }
-        super.onAttach(context);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.countries_list_frag, container, false);
     }
 
     public void onViewCreated(View view, Bundle savedInstanceState)  {
@@ -42,17 +28,34 @@ public class CountryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
+    //*******************************************************************
+    // After activity created we can set the adapter for the recycle view
+    //*******************************************************************
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        countriesAdapter = new CountriesAdapter(getActivity());
+        recyclerView.setAdapter(countriesAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-    public View onCreateView(View view, ViewGroup container,
-                             Bundle savedInstanceState) {
-        recyclerView = view.findViewById(R.id.recycle_view);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.countryRow_frag, container, false);
+    }
+
+
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        //this connect our mainactivity with the B fragment when the context var is the mainactivity
+        try{
+            this.listener = (CountriesFragmentListener)context;
+        }catch(ClassCastException e){}
+        super.onAttach(context);
     }
 
 
     //the interface of this fragment that include the methods
-    public interface CountryFragmentListener{
+    public interface CountriesFragmentListener{
         //put here methods you want to utilize to communicate with the hosting activity
     }
 }
+
